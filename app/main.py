8547dd0e84a7186aa9ac84f54df6ca5b5aa63818
KSilverman <ksilverman@umass.edu>
2018-10-13 16:04:01 -0400

    changed updates to properly upload files

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, flash, render_template, request, redirect, url_for
+from flask import Flask, flash, render_template, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = 'uploaded_data'
@@ -21,21 +21,27 @@ def index():
 def handle_data():
 	if request.method == 'POST':
 	    # check if the post request has the file part
-	    if 'file' not in request.files:
+	    if 'the_file' not in request.files:
 	        flash('No file part')
 	        return redirect(request.url)
-	    file = request.files['file']
+	    file = request.files['the_file']
 	    # if user does not select file, browser also
 	    # submit an empty part without filename
 	    if file.filename == '':
 	        flash('No selected file')
 	        return redirect(request.url)
-	    if file and allowed_file(file.filename):
+	    allowed = allowed_file(file.filename)
+	    if file and allowed:
 	        filename = secure_filename(file.filename)
 	        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-	        return redirect(url_for('uploaded_file',
-	                                filename=filename))
+	        return redirect(url_for('uploaded_file', filename=filename))
+	    else:
+	    	return render_template('index.html', auth=allowed)
 	return ''
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return 
 
 if __name__ == '__main__':
    app.run(debug = True)
